5d696ab9e247dcfe3dc584fb382146dc47f849bc
Ryan Lynch <ygd8@txstate.edu>
2023-12-04 15:29:04 -0600

    a

--- scale_images.py
+++ scale_images.py
@@ -1,8 +1,27 @@
 from PIL import Image
 import glob
+import cv2
+
 image_list = []
 path = "C:/Sync/CodingProjects/VisualStudioCode/CS-4337-Group-5-Final-Project/TensorFlow/workspace/training_demo/images"
 
-for filename in glob.glob(path + '/original/*.gif'): #assuming gif
-    im=Image.open(filename)
-    image_list.append(im)
+for filename in glob.glob(path + '/original/*.jpg'):
+    image = Image.open(filename)
+    image_list.append(image)
+
+top_width = 0
+top_height = 0
+
+for image in image_list:
+    width, height = image.size
+    
+    if width > top_width:
+        top_width = width
+
+    if height > top_height:
+        top_height = height
+
+output_path = path + '/resized/'
+
+for image in image_list:
+    
