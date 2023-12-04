21db9ba514a40cbb8a26ea4e717756a611f8a354
Ryan Lynch <ygd8@txstate.edu>
2023-12-04 14:39:05 -0600

    progress

--- extract_frames.py
+++ extract_frames.py
@@ -3,5 +3,21 @@ import os
 
 def extract_frames(video_filepath):
 
+    video = cv2.VideoCapture(video_filepath)
+
+    frames = []
+    frame_number = 0
+
+    while (True):
+        success, frame = video.read()
+
+        if success == True:
+            cv2.imwrite('/data/images'+str(frame_number)+'.jpg', frame)
+            frames.append(frame)
+        else:
+            break
+        frame_number += 1
+
+    video.release()
 
     return frames # frames is a list or array of frames in the video.
---
+++ main.py
@@ -0,0 +1,5 @@
+import extract_frames
+
+video_path = '/data/video/'
+
+frames = extract_frames(video_path)
