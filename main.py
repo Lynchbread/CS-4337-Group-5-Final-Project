310e76f44e8808a0d8e1893b83dcab981d29d56c
Jake <nkf17@txstate.edu>
2023-12-04 18:09:53 -0600

    Adds starting point for real-time video manipulation

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
 from extract_frames import extract_frames
 from detect_motion import detect_motion
 
@@ -29,11 +33,42 @@ from detect_motion import detect_motion
 
 def main():
 
+    # Hardcode video path for testing
     video_path = 'data\\videos\\The Deer Go Marching One by One [vZq88Iw8dsM].mp4'
+    # frames = extract_frames(video_path, 5)
 
-    frames = extract_frames(video_path, 5)
+    # Get video capture, fps, and delay value for playback at original speed
+    video = cv.VideoCapture(video_path)
+    fps = video.get(cv.CAP_PROP_FPS)
+    delay = int((1 / fps) * 1000) * 2
 
+    # Get first frame in video
+    frame_exists, current_frame = video.read()
+    
+    # Get list containing every frame in video
+    frames, i = extract_frames(video_path, 1), 0
+    
+    # Validate frame being correctly accessed from list
+    frames[i] = cv.cvtColor(frames[i], cv.COLOR_BGR2RGB)
+    plt.imshow(frames[len(frames) - 1])
+    plt.show()
 
+    # Loop until video ends (exit using 'ctrl+C' or simply 'q')
+    while frame_exists:
+
+        # Show current_frame
+        cv.imshow('frame', current_frame)
+
+        # Extract current frame
+        frame_exists, current_frame = video.read()
+
+        # Introduce delay, stop playback if 'q' pressed
+        if cv.waitKey(delay) & 0xFF == ord('q'):
+            break
+
+    # Exit program gracefully
+    video.release()
+    cv.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
