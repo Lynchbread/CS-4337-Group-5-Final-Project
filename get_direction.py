85a39d7f819b7de94bc545cda8c9de6ebb321af7
Ryan Lynch <ygd8@txstate.edu>
2023-12-06 21:52:30 -0600

    More comments

--- get_direction.py
+++ get_direction.py
@@ -3,40 +3,54 @@ import numpy as np
 
 # Returns True if getting closer
 # Returns False if stationary or moving away
-
-
 def get_direction(prev_object_centers, object_centers, road_contours):
 
+    # Returns false if there are no objects in the current or previous frame.
     if not prev_object_centers or not object_centers:
         return False
 
     prev_distances = []
     current_distances = []
 
+    # Get's a list of the shortest distance between the road and every object
+    # in the previous frame.
     for center in prev_object_centers:
         prev_distances.append(get_shortest_distance(center, road_contours))
 
+    # Get's a list of the shortest distance between the road and every object
+    # in the current frame.
     for center in object_centers:
         current_distances.append(get_shortest_distance(center, road_contours))
 
+    # Get's the distance of the object closest to the road in both the previous
+    # and current frames.
     prev_min = min(prev_distances)
     curr_min = min(current_distances)
 
+    # If the deer object is currently touching the road, or if the deer has moved
+    # 5 percent or more closer to the road compared to the previous frame, return True.
     if curr_min == 0.0 or prev_min / curr_min > 1.05:
         return True
     
+    # Returns false if the closest deer in the image is either stationary, or is
+    # moving away from the road.
     return False
 
-
+# Gets the shortest distance between the center point and any of the contours
+# in the image.
 def get_shortest_distance(center, contours):
 
     # Create a binary image to draw contours on
     image = np.zeros((500, 500), dtype=np.uint8)
+    # Draw the contours onto the image
     cv2.drawContours(image, contours, -1, (255), 1)
 
-    # Find the shortest distance
+    # Stores the shortest distance
     shortest_distance = np.float32('inf')
 
+    # Iterate throuh all contours and checks each center values' distance to
+    # the closest pixel of the contour. Compares all of the distances
+    # and keeps the shortest one.
     for contour in contours:
         distance = cv2.pointPolygonTest(contour, center, True)
         if distance < shortest_distance:
--- identify_objects.py
+++ identify_objects.py
@@ -4,17 +4,26 @@ import numpy as np
 
 def identify_objects(past_frame, current_frame, future_frame):
 
+    # Makes mask of the brown in the image
     mask = make_mask(current_frame)
 
+    # Makes binary image that contains only parts of the image that are in motion.
     binary_image = get_motion_image(past_frame, current_frame, future_frame, 10)
 
+    # Applies the mask onto the image. Any remaining white pixels means it is both
+    # in motion and brown.
     binary_image = cv2.bitwise_and(binary_image, binary_image, mask=mask)
 
+    # Removes any noise from image using erosion
     binary_image = cv2.erode(binary_image, np.ones((3, 3), np.uint8), iterations=1)
+    # Makes the deer objects larger to make up for the erosion we just did.
     binary_image = cv2.dilate(binary_image, np.ones((3, 3), np.uint8), iterations=9)
 
+    # Gets the corners of all rectangles for objects detected in the image
     rectangles = get_rectangles(binary_image, 1500)
-            
+
+    # If less than 2 objects are detected, repeats above steps but with a lower
+    # threshold for determining motion.   
     if len(rectangles) < 2:
         motion = get_motion_image(past_frame, current_frame, future_frame, 1)
         binary_image = cv2.bitwise_and(motion, motion, mask=mask)
@@ -25,14 +34,19 @@ def identify_objects(past_frame, current_frame, future_frame):
         
     areas = []
 
+    # Gets the area of all of the rectangles we just found and puts them in a list.
     for rec in rectangles:
         areas.append((rec, rec_area(rec[0], rec[1], rec[2], rec[3])))
 
+    # Sorts the areas in the list from largest to smallest.
     areas.sort(key=lambda x: x[1], reverse=True)
 
     i = 0
     object_centers = []
 
+    # Draws a rectangle around the 5 largest components in the image.
+    # Also determines the center of each of the 5 components and adds them to a list
+    # called object centers.
     for area in areas:
         if i < 5:
             object_centers.append(rec_center(area[0][0], area[0][1], area[0][2], area[0][3]))
@@ -41,12 +55,15 @@ def identify_objects(past_frame, current_frame, future_frame):
     
     return object_centers
 
-
 def get_rectangles(frame, threshold):
+    # Finds all connected components in the image. In this case, it should only be deer.
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(frame, connectivity = 4)
 
     rectangles = []
 
+    # Loops through the components. If a components' area is larger than 'threshold', determines the
+    # corners that form a rectangle around the entire component.
+    # Appends the corners to a list and returns the list.
     for i in range(1, nb_components):
         if stats[i, cv2.CC_STAT_AREA] > threshold:
             top_row = int(stats[i, cv2.CC_STAT_TOP])
@@ -58,12 +75,12 @@ def get_rectangles(frame, threshold):
 
     return rectangles
 
-
+# Extracts the color brown from the image and turns it into a binary mask.
 def make_mask(frame):
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    h = 0.5
-    s = 255
-    v = 255
+    h = 0.5     # h, s, and v are just for converting the standard HSV units of
+    s = 255     # (degrees 0 to 360, percent 0 to 100, percent 0 to 100)
+    v = 255     # into the form OpenCV uses which is (int 0 to 180, int 0 to 255, int 0 to 255)
     lower = np.array([0 * h, 0.1 * s, 0.2 * v], dtype=np.uint8)
     upper = np.array([60 * h, 0.25 * s, 0.5 * v], dtype=np.uint8)
     mask = cv2.inRange(hsv_frame, lower, upper)
@@ -72,15 +89,21 @@ def make_mask(frame):
 
 # Returns a binary image where any motion compared
 def get_motion_image(past_frame, current_frame, future_frame , thresh):
+    # Converts all 3 frames from BGR to Grayscale
     past_gray = cv2.cvtColor(past_frame, cv2.COLOR_BGR2GRAY)
     current_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
     future_gray = cv2.cvtColor(future_frame, cv2.COLOR_BGR2GRAY)
 
+    # Calculates the difference between the past and current frame
     diff1 = cv2.absdiff(past_gray, current_gray)
+    # Calculates the difference between the current and future frame
     diff2 = cv2.absdiff(future_gray, current_gray)
 
+    # Keeps the smallest differences from each diff1 and diff2
     motion = cv2.min (diff1, diff2)
 
+    # Thresholds image into a binary image in which if a difference is greater than 'thresh', it becomes white
+    # otherwise it's black.
     threshold, binary_image = cv2.threshold(motion, thresh = thresh, maxval = 255, type = cv2.THRESH_BINARY)
 
     return binary_image
