1649f14f5b5779be79c664b10a91f7cb474b0726
Jake <nkf17@txstate.edu>
2023-12-06 22:11:45 -0600

    Removes pseudocode

--- main.py
+++ main.py
@@ -5,32 +5,6 @@ from get_road_lines import get_road_lines
 from identify_objects import identify_objects
 from get_direction import get_direction
 
-    # Get first frame, set as 'prev'
-
-    # Vote on lines, top two should be bounds of road
-
-    # Create mask with only road lines
-    
-    # Store coordinates of each line as lists of tuples
-
-    # Start loop until no current frame exists
-
-    # Use 3 concurrent frames for motion detection (or more if needed)
-
-    # If motion detected and component originates within lines, ignore (Car)
-
-    # If motion detected originates outside lines, component above threshold, draw box
-
-    # Extract direction of motion (for instance, compare change in x, y )
-
-    # If motion towards road, hard signal (crossing, should linger after crossed)
-
-    # If motion along/ near road , signal (present)
-
-    # If no motion but components exist, signal (present)
-
-    # If no components (or car) no signal 
-
 def main():
 
     # Hardcode video path for testing
